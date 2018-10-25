f6c558afab124c9c4aa105bda24348912c230c80
Lucas Ernesto Kindinger <kindingerlek@hotmail.com>
2018-10-24 22:42:11 -0300

    reset the database

---
+++ BNETest/App_Data/dbDefinition.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Students] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Subjects] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (MAX) NULL,
+    [StudentID] INT            NULL,
+    CONSTRAINT [PK_Subjects] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Student] FOREIGN KEY ([StudentID]) REFERENCES [dbo].[Students] ([Id])
+);
+
